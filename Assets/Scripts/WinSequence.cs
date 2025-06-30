cc4f265586b0b833a5a46de5e4ba1a51d6c59053
Rafale25 <rafale25.dev@gmail.com>
2025-06-30 05:47:05 +0200

    Modified platforms to make ascending easier + reduced player's slipperyness

--- Assets/Scripts/WinSequence.cs
+++ Assets/Scripts/WinSequence.cs
@@ -10,6 +10,7 @@ public class WinSequence : MonoBehaviour
         if (collision.gameObject.TryGetComponent(out PlayerInput playerInput))
         {
             playerInput.enabled = false;
+            collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity *= 0.4f; // reduce velocity of player so he doesn't fall after the win condition
             TransitionManager.Instance.TransitionToScene(_nextSceneName, 1.5f);
         }
     }
